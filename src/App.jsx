3f5eb4ecfc790d2c4aefa8304d28d0bc91e7c831
rob587 <robertocamm02@yahoo.com>
2025-11-21 12:21:33 +0100

    prima milestone completata

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,43 @@
+import { useState, useEffect, useCallback } from "react";
+
 function App() {
-  return <></>;
+  const [data, setData] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+
+  useEffect(() => {
+    if (inputValue === "") {
+      setData([]);
+      return;
+    }
+
+    const url = `http://localhost:3333/products?search=${inputValue}`;
+
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setData(data);
+      });
+  }, [inputValue]);
+
+  return (
+    <>
+      <input
+        type="text"
+        placeholder="cerca prodotto"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+      />
+
+      {inputValue && data.length > 0 && (
+        <ul>
+          {data.map((product, i) => (
+            <li key={i}>{product.name}</li>
+          ))}
+        </ul>
+      )}
+    </>
+  );
 }
 
 export default App;
